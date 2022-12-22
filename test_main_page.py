8ba791889267b6ae2c0c1e647495e3a01464bcbf
Mikhail <ztx1213@gmail.com>
2022-12-22 22:29:07 +0300

    create class BasePage

--- test_main_page.py
+++ test_main_page.py
@@ -4,8 +4,12 @@ import time
 link = "http://selenium1py.pythonanywhere.com/"
 
 
+def go_to_login_page(browser):
+    browser.find_element(By.CSS_SELECTOR, "#login_link").click()
+
+
 def test_guest_can_go_to_login_page(browser):
     browser.implicitly_wait(10)
     browser.get(link)
-    time.sleep(10)
-    browser.find_element(By.CSS_SELECTOR, "#login_link").click()
+    time.sleep(1)
+    go_to_login_page(browser)
