fdf41aedfa71d67d80d3ac9619c940f08bffa150
Mikhail <ztx1213@gmail.com>
2022-12-23 09:02:42 +0300

    add lable_module_4.md

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,11 @@
-from pages.main_page import MainPage
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    browser.implicitly_wait(10)
+    browser.get(link)
+    time.sleep(10)
+    browser.find_element(By.CSS_SELECTOR, "#login_link").click()
