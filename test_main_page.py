c2892bfd36d0d0e56e6a4b369047771aa3541d16
Mikhail <ztx1213@gmail.com>
2022-12-27 23:59:51 +0300

    add class TestLoginFromMainPage

--- test_main_page.py
+++ test_main_page.py
@@ -9,19 +9,20 @@ link_1 = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
 
 
 # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-@pytest.mark.skip
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 @pytest.mark.skip
@@ -31,7 +32,6 @@ def test_should_be_login_page(browser):
     page.should_be_login_page()
 
 
-@pytest.mark.new
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link_2 = 'http://selenium1py.pythonanywhere.com/en-gb/'
     page = MainPage(browser, link_2)
